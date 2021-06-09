2e5bda1ab388b98e87be515f35ec67ae47a1d348
karthik <karthikenumarthi@gmail.com>
2021-06-09 15:14:56 +0530

    fix: port

--- index.js
+++ index.js
@@ -27,4 +27,4 @@ app.post('/subscribe',(req,res) => {
     })
 });
 
-app.listen(5001, console.log("server started on port: 5001"));
+app.listen(process.env.PORT, console.log("server started on port: 5001"));
