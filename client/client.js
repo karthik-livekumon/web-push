c7a911d77d781f8deeb4c7aaf7a6e6c1ca0a11e9
karthik <karthikenumarthi@gmail.com>
2021-06-10 13:11:36 +0530

    fix: send self notification

--- client/client.js
+++ client/client.js
@@ -1,6 +1,15 @@
-const getId =  async ()=> {
-    let data = await window.OneSignal.getUserId()
-    console.log(data)
-}
+const sendNotification = async () => {
+	let data = await window.OneSignal.getUserId();
+	fetch('https://onesignal.com/api/v1/notifications', {
+		method: 'POST',
+		body: JSON.stringify({
+			app_id: 'cf3dfb9a-f44e-4ec1-86c5-b36647660810',
+			contents: { en: 'Teacher is On Leave' },
+			include_player_ids: [data],
+			isAnyWeb: true,
+			headings: { en: 'Message From Livesloka' },
+			url: 'https://livesloka-web-push.herokuapp.com?_osp=do_not_open',
+		}),
+	});
+};
 
-getId()
